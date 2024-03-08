3812dfa35a5d2c4bb673b456380581e5dd44bf47
Эшли Карамель <biscuitisnotacookie@protonmail.com>
2024-03-08 13:10:00 +0000

    install flowbite-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<div class="flex flex-col items-center ">
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+<script>
+    import { Alert } from 'flowbite-svelte';
+</script>
+
+<div>
+    
+    <div class="flex flex-col items-center ">
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+
 </div>
